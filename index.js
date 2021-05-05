b744eff538a3d3898f4baa7fe282bb82c06c86a0
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-05 14:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solutions in this file
 function writeCards(array, name){
+  let newArray = [];
   for(let i=0; i<array.length; i++){
-    
+    newArray[i] = `Thank you, ${array[i]}, for the wonderful birthday gift!`;
   }
 }
