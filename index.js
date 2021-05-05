cb14331ab1751ecf7be3b5b437cf63e13f69c365
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-05 14:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(array, name){
   let newArray = [];
   for(let i=0; i<array.length; i++){
-    newArray[i] = `Thank you, ${array[i]}, for the wonderful birthday gift!`;
+    newArray[i] = `Thank you, ${array[i]}, for the wonderful surprise gift!`;
   }
   return newArray;
 }
